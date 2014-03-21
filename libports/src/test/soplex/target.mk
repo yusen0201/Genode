6a477b82f6f4a063dacc727618a2dbbac970617f
Johannes Schlatow <schlatow@ida.ing.tu-bs.de>
2014-03-21 14:06:58 +0100

    libports: fix test-soplex

--- libports/src/test/soplex/target.mk
+++ libports/src/test/soplex/target.mk
@@ -1,4 +1,6 @@
 TARGET   = test-soplex
 LIBS     = soplex libc_log stdcxx
-SOPLEX   = ../../../contrib/soplex-2.0.0
-SRC_CC   = $(SOPLEX)/src/example.cpp
+SOPLEX   = $(PRG_DIR)/../../../contrib/soplex-2.0.0/src/
+SRC_CC   = example.cpp
+
+vpath example.cpp $(SOPLEX)
