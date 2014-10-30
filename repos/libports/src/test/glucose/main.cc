de86b8a92f9a6c91d648e6a9dee3b6e361144b20
Johannes Schlatow <schlatow@ida.ing.tu-bs.de>
2014-10-30 19:01:35 +0100

    libports: add glucose test case
    Unfortunately, glucose is not working at the moment because of a
    segfault in ld.lib.so.
    This might have something to do with libc/stdcxx calls from glucose.

---
+++ repos/libports/src/test/glucose/main.cc
@@ -0,0 +1,23 @@
+#include <base/printf.h>
+
+#include <glucose/core/Solver.h>
+#include <glucose/core/Dimacs.h>
+
+using namespace Glucose;
+
+int main()
+{
+	Solver solver;
+
+	gzFile gzfile_components = gzopen("Test.cnf", "rb");	
+	if (gzfile_components != NULL) {
+		parse_DIMACS(gzfile_components, solver);
+		gzclose(gzfile_components);
+	}
+	else {
+		PERR("Unable to open file \"Test.cnf\"");
+		return 1;
+	}
+
+	return 0;
+}
--- repos/libports/src/test/libports/glucose/target.mk
+++
@@ -1,5 +0,0 @@
-TARGET = test-glucose
-LIBS   = glucose
-SRC_CC = main.cc
-
-vpath main.cc $(PRG_DIR)/..
